a94ef0f61b07e2d5904314e61222260a18c9e081
nikhil kumar <nikkm2000@gmail.com>
2023-01-10 11:45:27 +0530

    first

---
+++ src/test/java/com/test/myAppTest/App1Test.java
@@ -0,0 +1,25 @@
+package com.test.myAppTest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.main.myApp.App1;
+
+public class App1Test {
+
+	@Test
+	public void positiveLoginCredential() {		
+		App1 a1= new  App1();
+		int myreturnValues= a1.m("nikhil1", "password1");	
+		Assert.assertEquals(myreturnValues, 1 );		
+	}
+
+	@Test
+	public void negativeLoginCredential() {		
+		App1 a1= new  App1();
+		int myreturnValues= a1.m("nik1", "pass1");		 
+		Assert.assertEquals(0,myreturnValues );		
+	}
+
+
+}
